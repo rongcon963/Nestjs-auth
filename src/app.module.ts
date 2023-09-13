711d87b0bf83d12f190de7d96ac9e80ae7ddb4e5
Tuan Pham <rongcon963@yahoo.com>
2023-09-13 16:01:43 +0700

    Add migration db, config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/entities/user.entity';
+import typeorm from './config/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'user',
-      entities: [User],
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm]
     }),
-    UserModule
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => (configService.get('typeorm'))
+    }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
